c3462c026e1e56b69852125af743fa5c35e17471
Travis Kosarek <travis.kosarek@gmail.com>
2018-01-31 00:04:53 -0600

    added source field to retrieve source build repo

--- src/git-mirror/git-mirror-task.ts
+++ src/git-mirror/git-mirror-task.ts
@@ -2,18 +2,18 @@ import * as taskLib from 'vsts-task-lib';
 
 export class GitMirrorTask {
 
-    private gitMirrorRepositoryUri: string;
+    private destinationGitRepositoryUri: string;
     private gitMirrorPersonalAccessToken: string;
     private sourceGitRepositoryUri: string;
 
     public constructor() {
         try {
-            this.gitMirrorRepositoryUri = taskLib.getInput('gitMirrorRepositoryUri', true);
+            this.sourceGitRepositoryUri = taskLib.getInput('sourceGitRepositoryUri', true);
+            this.destinationGitRepositoryUri = taskLib.getInput('destinationGitRepositoryUri', true);
             this.gitMirrorPersonalAccessToken = taskLib.getInput('gitMirrorPersonalAccessToken', true);
-            this.sourceGitRepositoryUri = taskLib.getInput('$(Build.Repository.Uri)', true);
         }
         catch (e) {
-            //
+            taskLib.setResult(taskLib.TaskResult.Failed, e);
         }
     }
 
@@ -22,14 +22,13 @@ export class GitMirrorTask {
             // check if git exists as a tool
             taskLib.which('git', true);
 
+            // this.gitCloneMirror();
+            // this.gitPushMirror();
 
             console.log('********* ' + this.sourceGitRepositoryUri);
         }
         catch (e) {
             taskLib.setResult(taskLib.TaskResult.Failed, e);
-        }
-        finally {
-            // this.onComplete();
         }
     }
 
@@ -37,21 +36,30 @@ export class GitMirrorTask {
         taskLib.tool('git')
                 .arg('clone')
                 .arg('--mirror')
-                .arg('$(Build.Repository.Uri)')
+                .arg(this.sourceGitRepositoryUri)
                 .exec();
     }
 
     private gitPushMirror() {
-        //
+        taskLib.tool('git')
+                .arg('-C')
+                .arg(this.getSourceGitFolder(this.sourceGitRepositoryUri))
+                .arg('push')
+                .arg('--mirror')
+                .arg(this.getAuthenticatedGitUri(this.destinationGitRepositoryUri, this.gitMirrorPersonalAccessToken))
+                .exec();
     }
 
-    private onComplete() {
-        try {
-            //
-        }
-        catch (e) {
-            taskLib.setResult(taskLib.TaskResult.Failed, e);
-        }
+    private getSourceGitFolder(uri: string): string {
+        //
+
+        return '';
+    }
+
+    private getAuthenticatedGitUri(uri: string, token: string): string {
+        //
+
+        return '';
     }
 }
 
